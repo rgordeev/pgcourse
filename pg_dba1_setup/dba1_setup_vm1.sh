5fa963386d760e9bea413f232bc5be040ad83ef6
rgordeev <rgordeev@naumen.ru>
2015-12-16 11:39:34 +0300

    fix dba1_setup_vm1.sh

--- pg_dba1_setup/dba1_setup_vm1.sh
+++ pg_dba1_setup/dba1_setup_vm1.sh
@@ -1,4 +1,4 @@
 sudo apt-get install libreadline6 libreadline6-dev zlib1g zlib1g-dev docbook docbook-dsssl docbook-xsl openjade1.3 opensp xsltproc
-mkdir -p /home/student/DBA1/practice
-chmod 755 /home/student/DBA1
-chmod 777 /home/student/DBA1/practice
+mkdir -p $HOME/DBA1/practice
+chmod 755 $HOME/DBA1
+chmod 777 $HOME/DBA1/practice
